ba50b5988a5af267b0aa05a6dce0540cdb49fc47
juerbau <juerbau@web.de>
2026-01-03 15:27:14 +0100

    final version

--- app/dashboard/(crm)/customers/page.tsx
+++ app/dashboard/(crm)/customers/page.tsx
@@ -1,6 +1,6 @@
-import CustomersTable from '@/app/ui/customers/table';
+// import CustomersTable from '@/app/ui/customers/table';
 import {Metadata} from "next";
-import {fetchCustomers} from "@/app/lib/data";
+// import {fetchCustomers} from "@/app/lib/data";
 
 export const metadata: Metadata = {
   title: 'Customers',
@@ -8,7 +8,5 @@ export const metadata: Metadata = {
 
 export default async function Page() {
 
-  const customers = await fetchCustomers();
-
-  return <CustomersTable customers={customers} />;
+  return <h1>Customers coming soon...</h1>;
 }
